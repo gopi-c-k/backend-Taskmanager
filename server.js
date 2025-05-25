d3424873fdc55cb7129a3b40da150f86b1e1e5e2
Gopi C K <gopick2004@gmail.com>
2025-05-25 20:23:02 +0530

    U: Server.js C: Routes - userRoute,taskRoute,authRoute

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
+const cookieParser = require('cookie-parser');
+const connectDB = require('./src/config/db')
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const userRoutes = require('./src/routes/userRoute');
+const taskRoutes = require('./src/routes/taskRoute');
+const authRoutes = require('./src/routes/userRoute');
 
 dotenv.config();
+connectDB();
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/api', (req, res) => {
   res.send('API working Properly');
 });
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error(err));
+// Route
+app.use("/user", userRoutes);
+app.use("/auth",authRoutes);
+app.use("/task",taskRoutes);
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on available port ${process.env.PORT || 5000}`);
