f0e46d8dfdd41601e8c93f14e9c4d413ba9c13b6
Gopi C K <gopick2004@gmail.com>
2025-05-25 21:59:14 +0530

    C: Model - invite

---
+++ src/models/invite.js
@@ -0,0 +1,11 @@
+const inviteSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    token: { type: String, required: true },
+    role: { type: String, enum: ['Manager', 'Member'], required: true },
+    organization: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true },
+    invitedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    status: { type: String, enum: ['Pending', 'Accepted'], default: 'Pending' },
+    expiresAt: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('Invite', inviteSchema);
